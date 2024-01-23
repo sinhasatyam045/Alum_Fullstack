79f98344bf69de6c3f19c821a4e1a0197c42826f
Abhijeet Kumar <abhijeetvr12@gmail.com>
2024-01-23 23:14:39 +0530

    feedbackkPage Added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Dashboard from './components/dashboard/dashboard';
 import Login from './components/login/AlumniLogin';
 import Notification from './components/notification/notification';
+import Feedback from './components/forms/feedbackForm'
 
 function App() {
   return (
@@ -10,7 +11,9 @@ function App() {
         <Routes>
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/" element={<Login />} />
-          <Route path="/notification" element={<Notification />} />  
+          <Route path="/notification" element={<Notification />} /> 
+          <Route path="/feedback" element={<Feedback />} /> 
+           
         </Routes>
       </div>
     </Router>
