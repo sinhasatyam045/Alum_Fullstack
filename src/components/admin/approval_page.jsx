ff8d9f6a7b5c0f935b0215c0736be278c6a5f8d2
Cinemato <bpawan2002@gmail.com>
2024-01-24 18:30:37 +0530

    Alumni approval page fixes

--- src/components/admin/approval_page.jsx
+++ src/components/admin/approval_page.jsx
@@ -4,54 +4,99 @@ import ellipse1 from './ellipse1.png';
 import ellipse2 from './ellipse2.png';
 import businessman from './businessman.png';
 import person from './person.png';
+import NavBar from '../helper/navbar';
+
 
 const approval_page = () => {
   return (
     <>
-    <div className='w-full h-16 bg-blue-900'></div>
-    <div className='static'>
-      <img src={ellipse1} style={{height: "500px"}} className='absolute top-15 right-0'/>
-      <img src={ellipse2} style={{height: "350px"}} className='absolute bottom-0 right-0'/>
-      <img src={businessman} style={{height: "350px"}} className='absolute top-28 right-12'/>
+      <div className='w-5/6 mx-auto'>
+
+
+        {/* <div className='w-full h-16 bg-blue-900'></div> */}
+        <div >
+          <NavBar></NavBar>
+        </div>
+        <div className='static'>
+          <img src={ellipse1} style={{ height: "500px" }} className='absolute top-15 right-0' />
+          <img src={ellipse2} style={{ height: "350px" }} className='absolute bottom-0 right-0' />
+          <img src={businessman} style={{ height: "350px" }} className='absolute top-28 right-12' />
 
 
 
-    </div>  
-    <div className='px-24 pt-16'>
+        </div>
+        <div className='px-24 pt-16'>
 
-    
-    <div className='text-6xl text-gray-500 pb-6'>Approval Page</div>
-    <div className='text-4xl text-blue-900 '>Alumni</div>
-    
-    <div className='h-0.5 w-28 bg-black mb-10'></div>
-    <div className='flex justify-start'>
-      <div className='text-2xl pr-40 pt-12'>
-        <div className='py-2'>Name: Navin Sharma</div>
-        <div className='py-2'>User Name: navinsharma30</div>
-        <div className='py-2'>Registration number: 21E0880</div>
+
+          <div className='text-6xl text-gray-500 font-bold pb-6'>Approval Page</div>
+          <div className='text-4xl text-blue-900 font-semibold'>Alumni</div>
+
+          <div className='h-0.5 w-28 bg-black mb-10'></div>
+
+        </div>
+
+        <AlumniCards
+          regNo="2018B4A70633G"
+          UserName="johndoe"
+          DisplayName="John Doe"
+        />
+
+        <div className='flex justify-center pt-14'>
+
+          <img src={tick} alt='tickmark' className='h-10 w-24' />
+          <img src={tick} alt='tickmark' className='h-10 w-24' />
+          <img src={tick} alt='tickmark' className='h-10 w-24' />
+
+        </div>
       </div>
-      <div className='pb-20 pl-32'>
-        <img src={person} className='max-h-44 '/>
-      </div>
-    </div>
-    </div>
-    <div className='flex justify-center'>
-      <div className='px-28'>
-      <button className=' px-20 bg-green-700 py-5 text-2xl'>Approve</button>
-      </div>
-      <div className='px-28'>
-      <button className='px-24 bg-red-700 py-5 text-2xl'>Deny</button>
-      </div>
-    </div>
-    <div className='flex justify-center pt-14'>
-    
-      <img src={tick} alt='tickmark' className='h-10 w-24'/>
-      <img src={tick} alt='tickmark' className='h-10 w-24'/>
-      <img src={tick} alt='tickmark' className='h-10 w-24'/>
-    
-    </div>
     </>
   )
+}
+
+
+
+
+
+const AlumniCards = (props) => {
+  return (
+    <>
+      <div className='w-3/6 mx-auto bg-white p-8 rounded-lg shadow-lg'>
+        <div className='flex justify-between items-center'>
+          <div className='text-xl'>
+            <table className='border-collapse w-full p-4'>
+              <tr>
+                <td className='p-3 border text-sm text-center font-semibold text-gray-700'>Registration number</td>
+                <td className='p-3 border text-sm  text-gray-700 '>{props.regNo}</td>
+              </tr>
+              <tr>
+                <td className='p-3 border text-sm text-center font-semibold text-gray-700'>User Name</td>
+                <td className='p-3 border text-sm text-gray-800'>{props.UserName}</td>
+              </tr>
+              <tr>
+                <td className='p-3 border text-sm text-center font-semibold text-gray-700'>Name</td>
+                <td className='p-3 border text-sm text-gray-800'>{props.DisplayName} </td>
+              </tr>
+
+            </table>
+          </div>
+
+          <div className='flex-shrink-0'>
+            <img src={person} className='max-h-44 rounded-lg' alt='Person' />
+          </div>
+        </div>
+
+        <div className='flex justify-center mt-8'>
+          <div className='px-4'>
+            <button className='px-6 bg-green-700 py-3 text-lg text-white rounded-lg'>Approve</button>
+          </div>
+          <div className='px-4'>
+            <button className='px-6 bg-red-700 py-3 text-lg text-white rounded-lg'>Deny</button>
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
 }
 
 export default approval_page
