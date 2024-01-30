d7363c17d945167b2279547e46b87cf85b4abf53
Abhijeet Kumar <abhijeetvr12@gmail.com>
2024-01-31 01:00:29 +0530

    onBoarding

--- src/components/onBoardings/screenOne.jsx
+++ src/components/onBoardings/screenOne.jsx
@@ -7,48 +7,44 @@ const ScreenOne = () => {
 
     const handleStartClick = () => {
         navigate('/onboarding2');
-    }
+    };
+
     return (
         <>
-        <div className='h-screen overflow-hidden'>
-        <Navbar />
-         <div className='w-full h-screen text-gray-100 bg-[#1f2e44] overflow-hidden'>
-           
-            <div className='flex flex-row gap-4 justify-between items-center'>
-                <div className='flex justify-center items-center'>
-                    <img src="Images/LoginImages/StudentImage.png" alt="" srcset="" />
+            <div className='overflow-hidden'>
+                <Navbar />
+                <div className='w-full min-h-screen text-gray-100 bg-[#1f2e44] overflow-hidden flex flex-col lg:flex-row'>
 
-                </div>
-                <div className='h-screen flex flex-col justify-between py-3 items-center'>
-                    <div className='my-auto flex flex-col gap-12 items-center'>
-                        <div className='flex flex-col justify-center items-center'>
-                            <h1 className='font-bold p-3'>AlumnUnity</h1>
-                            <p className='text-gray-500'>Connecting the School community</p>
-                        </div>
-                        <div><img src="Images/logoHome.png" alt="" /></div>
-                        <div className='flex flex-col justify-center items-center' >
-                            <h3 className='font-semibold text-xl text-center p-4'>Let's get Started</h3>
-                            <button
-                            className='bg-indigo-900'
-                            onClick={handleStartClick}
-                            >Start</button>
-                        </div>
+                    <div className='lg:w-1/3 flex justify-center items-center image1'>
+                        <img className='lg:h-[60vh] m-8' src="Images/LoginImages/StudentImage.png" alt="" />
+                    </div>
 
+                    <div className='lg:w-1/3 flex flex-col justify-between py-3 items-center mt-0 title'>
+                        <div className='my-auto flex flex-col gap-12 items-center'>
+                            <div className='flex flex-col justify-center items-center'>
+                                <h1 className='font-bold p-3'>AlumnUnity</h1>
+                                <p className='text-gray-500'>Connecting the School community</p>
+                            </div>
+                            <div><img className='lg:h-[40vh]' src="Images/logoHome.png" alt="" /></div>
+                            <div className='flex flex-col justify-center items-center'>
+                                <h3 className='font-semibold text-xl text-center p-4'>Let's get Started</h3>
+                                <button
+                                    className='bg-indigo-900'
+                                    onClick={handleStartClick}
+                                >
+                                    Start
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className='lg:w-1/3 flex justify-center items-center image2'>
+                        <img className='lg:h-[60vh] mt-8' src="Images/LoginImages/AlumniImage.png" alt="" />
                     </div>
 
                 </div>
-                <div>
-                    <img src="Images/LoginImages/AlumniImage.png" alt="" />
-                </div>
             </div>
-
-        </div>
-
-        </div>
-        
         </>
-       
-
     );
 };
 
