4679ef26d6c5f2f8d174911a590a0dfef154f737
Risshab Singla <risshab2singla@gmail.com>
2024-02-05 12:19:16 +0530

    Navbar indentation

--- src/components/helper/navbar.jsx
+++ src/components/helper/navbar.jsx
@@ -5,8 +5,10 @@ import { useNavigate } from "react-router-dom";
 const Navbar = () => {
   const navigate = useNavigate();
   // bg-[#F8F9FA]
-  const aboveNavBarListClass = "flex p-1  text-indio-900 hover:text-indigo-500 cursor-pointer border-r-2"
-  const lowerNavBarListClass = "flex px-2 text-indio-900 hover:text-indigo-500 cursor-pointer"
+  const aboveNavBarListClass =
+    "flex p-1 text-indio-900 hover:text-indigo-500 cursor-pointer border-r-2";
+  const lowerNavBarListClass =
+    "flex px-2 text-indio-900 hover:text-indigo-500 cursor-pointer";
   return (
     <header className="bg-transparent  mx-auto">
       <div className="container mx-auto flex justify-between items-center">
@@ -14,114 +16,169 @@ const Navbar = () => {
           <img className="" src="Images/logo.png" alt="" />
         </div>
         <div>
-          
-        <nav>
-          <ul className="flex text-[0.6rem] justify-end">
-                <li className={aboveNavBarListClass}
-                onClick={()=>{navigate("/")}}
-                // TODO Replace with the required path 
-                >
-                  <img className="px-1" src="./src/assets/svgs/college_graduation.svg"></img>
-                  <p className="p-1"> ADMISSION FORM</p>
-                </li>
-                <li className={aboveNavBarListClass}
-                onClick={()=>{navigate("/")}}
-                 // TODO Replace with the required path 
-                >
-                  <img className="px-1" src="./src/assets/svgs/phone.svg"></img>
-                  <p className="p-1"> CONTACT US</p>
-                </li>
-                <li className={aboveNavBarListClass}
-                onClick={()=>{navigate("/")}}
-                 // TODO Replace with the required path 
-                >
-                  <img className="px-1" src="./src/assets/svgs/people_2.svg"></img>
-                  <p className="p-1"> PARENT'S LOGIN</p>
-                </li>
-                <li className={aboveNavBarListClass}
-                onClick={()=>{navigate("/")}}
-                 // TODO Replace with the required path 
-                >
-                  <img className="px-1" src="./src/assets/svgs/people_2.svg"></img>
-                  <p className="p-1">ADMIN LOGIN</p>
-                </li>
-                <li className={aboveNavBarListClass}
-                onClick={()=>{navigate("/")}}
-                 // TODO Replace with the required path 
-                >
-                  <img className="px-1" src="./src/assets/svgs/suitcase.svg"></img>
-                  <p className="p-1">CAREERS</p>
-                </li>
-                <li className={aboveNavBarListClass}
-                onClick={()=>{navigate("/")}}
-                >
-                  <img className="px-1" src="./src/assets/svgs/assignment.svg"></img>
-                  <p className="p-1">MANDATORY PUBLIC DISCLOSURE</p>
-                </li>
-                
-                {/* <li className="font-semibold px-2 text-indio-900  cursor-pointer flex align-center">
+          <nav>
+            <ul className="flex text-[0.6rem] justify-end">
+              <li
+                className={aboveNavBarListClass}
+                onClick={() => {
+                  navigate("/");
+                }}
+                // TODO Replace with the required path
+              >
+                <img
+                  className="px-1"
+                  src="./src/assets/svgs/college_graduation.svg"
+                ></img>
+                <p className="p-1"> ADMISSION FORM</p>
+              </li>
+              <li
+                className={aboveNavBarListClass}
+                onClick={() => {
+                  navigate("/");
+                }}
+                // TODO Replace with the required path
+              >
+                <img className="px-1" src="./src/assets/svgs/phone.svg"></img>
+                <p className="p-1"> CONTACT US</p>
+              </li>
+              <li
+                className={aboveNavBarListClass}
+                onClick={() => {
+                  navigate("/");
+                }}
+                // TODO Replace with the required path
+              >
+                <img
+                  className="px-1"
+                  src="./src/assets/svgs/people_2.svg"
+                ></img>
+                <p className="p-1"> PARENT'S LOGIN</p>
+              </li>
+              <li
+                className={aboveNavBarListClass}
+                onClick={() => {
+                  navigate("/");
+                }}
+                // TODO Replace with the required path
+              >
+                <img
+                  className="px-1"
+                  src="./src/assets/svgs/people_2.svg"
+                ></img>
+                <p className="p-1">ADMIN LOGIN</p>
+              </li>
+              <li
+                className={aboveNavBarListClass}
+                onClick={() => {
+                  navigate("/");
+                }}
+                // TODO Replace with the required path
+              >
+                <img
+                  className="px-1"
+                  src="./src/assets/svgs/suitcase.svg"
+                ></img>
+                <p className="p-1">CAREERS</p>
+              </li>
+              <li
+                className={aboveNavBarListClass}
+                onClick={() => {
+                  navigate("/");
+                }}
+              >
+                <img
+                  className="px-1"
+                  src="./src/assets/svgs/assignment.svg"
+                ></img>
+                <p className="p-1">MANDATORY PUBLIC DISCLOSURE</p>
+              </li>
+
+              {/* <li className="font-semibold px-2 text-indio-900  cursor-pointer flex align-center">
                   <Mode_btn />
                 </li> */}
-              </ul>
-            </nav>
-          
-        <nav className="pt-2">
-          <ul className="flex space-x-4 items-center">
-          <li className={lowerNavBarListClass}
-            onClick={()=>{navigate("/")}}
-             // TODO Replace with the required path 
-            >
-            <p className="p-1"> About Us</p>
-          </li>
-          <li className={lowerNavBarListClass}
-            onClick={()=>{navigate("/")}}
-             // TODO Replace with the required path 
-            >
-            <p className="p-1"> Campus</p>
-          </li>
-          <li className={lowerNavBarListClass}
-            onClick={()=>{navigate("/")}}
-             // TODO Replace with the required path 
-            >
-            <p className="p-1"> Academics</p>
-          </li>
-          <li className={lowerNavBarListClass}
-            onClick={()=>{navigate("/")}}
-             // TODO Replace with the required path 
-            >
-            <p className="p-1"> Co-Curricular</p>
-          </li>
-          <li className={lowerNavBarListClass}
-            onClick={()=>{navigate("/")}}
-             // TODO Replace with the required path 
-            >
-            <p className="p-1"> School Life</p>
-          </li>
-          <li className={lowerNavBarListClass}
-            onClick={()=>{navigate("/")}}
-             // TODO Replace with the required path 
-            >
-            <p className="p-1"> Admission</p>
-          </li>
-          <li className={lowerNavBarListClass}
-            onClick={()=>{navigate("/")}}
-             // TODO Replace with the required path 
-            >
-            <p className="p-1"> News & Updates</p>
-          </li>
-          <li className={lowerNavBarListClass}
-            onClick={()=>{navigate("/")}}
-             // TODO Replace with the required path 
-            >
-            <p className="p-1"> AlumUnity</p>
-          </li>
-            {/* <li className="font-semibold px-2 text-indio-900  cursor-pointer flex align-center">
+            </ul>
+          </nav>
+
+          <nav className="pt-2">
+            <ul className="flex space-x-4 items-center">
+              <li
+                className={lowerNavBarListClass}
+                onClick={() => {
+                  navigate("/");
+                }}
+                // TODO Replace with the required path
+              >
+                <p className="p-1"> About Us</p>
+              </li>
+              <li
+                className={lowerNavBarListClass}
+                onClick={() => {
+                  navigate("/");
+                }}
+                // TODO Replace with the required path
+              >
+                <p className="p-1"> Campus</p>
+              </li>
+              <li
+                className={lowerNavBarListClass}
+                onClick={() => {
+                  navigate("/");
+                }}
+                // TODO Replace with the required path
+              >
+                <p className="p-1"> Academics</p>
+              </li>
+              <li
+                className={lowerNavBarListClass}
+                onClick={() => {
+                  navigate("/");
+                }}
+                // TODO Replace with the required path
+              >
+                <p className="p-1"> Co-Curricular</p>
+              </li>
+              <li
+                className={lowerNavBarListClass}
+                onClick={() => {
+                  navigate("/");
+                }}
+                // TODO Replace with the required path
+              >
+                <p className="p-1"> School Life</p>
+              </li>
+              <li
+                className={lowerNavBarListClass}
+                onClick={() => {
+                  navigate("/");
+                }}
+                // TODO Replace with the required path
+              >
+                <p className="p-1"> Admission</p>
+              </li>
+              <li
+                className={lowerNavBarListClass}
+                onClick={() => {
+                  navigate("/");
+                }}
+                // TODO Replace with the required path
+              >
+                <p className="p-1"> News & Updates</p>
+              </li>
+              <li
+                className={lowerNavBarListClass}
+                onClick={() => {
+                  navigate("/");
+                }}
+                // TODO Replace with the required path
+              >
+                <p className="p-1"> AlumUnity</p>
+              </li>
+              {/* <li className="font-semibold px-2 text-indio-900  cursor-pointer flex align-center">
               <Mode_btn />
             </li> */}
-          </ul>
-        </nav>
+            </ul>
+          </nav>
         </div>
-        
       </div>
     </header>
   );
