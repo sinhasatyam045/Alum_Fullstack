5a5c638b4a45869d9d95c0d179b23f1d603b0a62
Risshab Singla <risshab2singla@gmail.com>
2024-03-25 12:35:29 +0530

    Message Console Logs

--- src/components/message/messageBoard.jsx
+++ src/components/message/messageBoard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { FiBell } from "react-icons/fi";
 import { FaSearch } from "react-icons/fa";
 import ChatSnippet from "./ChatSnippet";
@@ -8,55 +8,85 @@ import ProfilePicture from "../../assets/images/message/profile.png";
 import NavigationPanel from "./NavigationPanel";
 import { IoIosSend } from "react-icons/io";
 
-const messageBoard = () => {
-  const chatList = [
-    {
-      profileImage: null,
-      name: "John Doe",
-      Designation: "Student",
-    },
-    {
-      profileImage: null,
-      name: "Jane Smith",
-      Designation: "Alumni",
-    },
-    {
-      profileImage: null,
-      name: "Ajax",
-      Designation: "Student",
-    },
-    {
-      profileImage: null,
-      name: "Joeline",
-      Designation: "Faculty",
-    },
-    {
-      profileImage: null,
-      name: "Nadira Sam",
-      Designation: "Faculty",
-    },
-    {
-      profileImage: null,
-      name: "Hagrid Hash",
-      Designation: "Alumni",
-    },
-    // Add more chat data as needed
-  ];
+const chatList = [
+  {
+    profileImage: null,
+    name: "John Doe",
+    Designation: "Student",
+  },
+  {
+    profileImage: null,
+    name: "Jane Smith",
+    Designation: "Alumni",
+  },
+  {
+    profileImage: null,
+    name: "Ajax",
+    Designation: "Student",
+  },
+  {
+    profileImage: null,
+    name: "Joeline",
+    Designation: "Faculty",
+  },
+  {
+    profileImage: null,
+    name: "Nadira Sam",
+    Designation: "Faculty",
+  },
+  {
+    profileImage: null,
+    name: "Hagrid Hash",
+    Designation: "Alumni",
+  },
+  // Add more chat data as needed
+];
 
+const MessageBoard = () => {
   const [messages, setMessages] = useState([]);
   const [newMessage, setNewMessage] = useState("");
   const [searchText, setSearchText] = useState("");
   const [clickMessage, setclickMessage] = useState("");
+  const [chatOpened, setChatOpended] = useState({});
 
   const handleSendMessage = () => {
     if (newMessage.trim() !== "") {
+      const obj = {
+        status: "messageSend",
+        message: newMessage,
+        chatOpened,
+      };
+      console.log(obj);
       setMessages([...messages, { text: newMessage, sender: "me" }]);
       setNewMessage("");
     }
   };
-  const handleChatClick = (clickedName) => {
-    setclickMessage(clickedName);
+
+  const handleChatClick = (e, chat) => {
+    if (chat === chatOpened) {
+      return;
+    }
+    const obj = {
+      status: "chatOpened",
+      chat,
+    };
+    setChatOpended(chat);
+    console.log(obj);
+    setclickMessage(chat.name);
+    setMessages([]);
   };
+
+  function handleEscPress(e) {
+    // console.log(e);
+    if (e.key !== "Escape") {
+      return;
+    }
+    setclickMessage("");
+  }
+  useEffect(() => {
+    window.addEventListener("keydown", handleEscPress);
+    return () => window.removeEventListener("keydown", handleEscPress);
+  }, []);
   return (
     <div className="flex flex-col h-screen">
       {/* Header */}
@@ -123,59 +153,61 @@ const messageBoard = () => {
                 profileImage={chat.profileImage}
                 name={chat.name}
                 designation={chat.Designation}
-                onClick={handleChatClick}
+                onClick={(e) => handleChatClick(e, chat)}
               />
             ))}
           </div>
         </div>
 
         {/* Chat Panel */}
-        <div className="md:w-3/5 sm:w-full md:flex-1 flex flex-col">
-          <div
-            style={{ backgroundColor: "#204C89" }}
-            className="h-16 p-1 flex items-center"
-          >
-            <img src={NoProfile} className="w-12 h-12"></img>
-            <h2 className="text-white ml-4">{clickMessage}</h2>
-          </div>
-          <div className="p-4 flex-1 overflow-y-scroll">
-            {messages.map((message, index) => (
-              <div
-                key={index}
-                className={`mb-4 ${
-                  message.sender === "me" ? "text-right" : "text-left"
-                }`}
-              >
-                <span
-                  style={{ backgroundColor: "rgb(22, 53, 96)" }}
-                  className={`inline-block p-2 bg-blue-500 rounded-lg text-white`}
+        {clickMessage !== "" && (
+          <div className="md:w-3/5 sm:w-full md:flex-1 flex flex-col">
+            <div
+              style={{ backgroundColor: "#204C89" }}
+              className="h-16 p-1 flex items-center"
+            >
+              <img src={NoProfile} className="w-12 h-12"></img>
+              <h2 className="text-white ml-4">{clickMessage}</h2>
+            </div>
+            <div className="p-4 flex-1 overflow-y-scroll">
+              {messages.map((message, index) => (
+                <div
+                  key={index}
+                  className={`mb-4 ${
+                    message.sender === "me" ? "text-right" : "text-left"
+                  }`}
                 >
-                  {message.text}
-                </span>
+                  <span
+                    style={{ backgroundColor: "rgb(22, 53, 96)" }}
+                    className={`inline-block p-2 bg-blue-500 rounded-lg text-white`}
+                  >
+                    {message.text}
+                  </span>
+                </div>
+              ))}
+            </div>
+            <div className="flex p-2">
+              <div className="flex px-4 w-full mx-auto gap-2">
+                <input
+                  type="text"
+                  value={newMessage}
+                  onChange={(e) => setNewMessage(e.target.value)}
+                  placeholder="Type your message..."
+                  className="w-full p-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                />
+                <button
+                  onClick={handleSendMessage}
+                  className="bg-blue-500 px-4 py-2 text-white rounded-md focus:outline-none focus:ring focus:border-blue-300"
+                >
+                  <IoIosSend className="text-white" />
+                </button>
               </div>
-            ))}
-          </div>
-          <div className="flex p-2">
-            <div className="flex px-4 w-full mx-auto gap-2">
-              <input
-                type="text"
-                value={newMessage}
-                onChange={(e) => setNewMessage(e.target.value)}
-                placeholder="Type your message..."
-                className="w-full p-2 border rounded-md focus:outline-none focus:ring focus:border-blue-300"
-              />
-              <button
-                onClick={handleSendMessage}
-                className="bg-blue-500 px-4 py-2 text-white rounded-md focus:outline-none focus:ring focus:border-blue-300"
-              >
-                <IoIosSend className="text-white" />
-              </button>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
 };
 
-export default messageBoard;
+export default MessageBoard;
